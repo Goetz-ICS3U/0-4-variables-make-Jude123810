f39646398d74796a91b61fb2c52be3d654bff383
Jude123810 <354232233@educ.dpcdsb.org>
2026-02-26 12:13:46 -0500

    Add files via upload

--- vars_make.py
+++ vars_make.py
@@ -13,7 +13,7 @@ width = int(input("Width:"))
 side_length = int(input("Side length:"))
 
 #Processing 
-area_circle = (math.pi) *(radius**2) 
+area_circle = math.pi *radius**2
 per_circle = 2*math.pi*radius
 
 
@@ -25,6 +25,7 @@ per_rect = 2*(length+width)
 oct_area = 2*(1+math.sqrt(2))*side_length**2
 per_oct = 8*side_length 
 
-print(f"The circle has an area of{area_circle} and a perimeter of{per_circle}")
-print(f"The rectangle has an area of{area_rect} and a perimeter of {per_rect}")
+#Output
+print(f"The circle has an area of {area_circle} and a perimeter of {per_circle}")
+print(f"The rectangle has an area of {area_rect} and a perimeter of {per_rect}")
 print(f"The octagon has an area of {oct_area} and a perimeter of {per_oct}")
